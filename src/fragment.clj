835349953aa07511df364f1ca880d124ee5b3d88
Bina <binasteinert@gmail.com>
2019-01-05 13:47:05 +0100

    Inital commit for clojure fragment

---
+++ src/fragment.clj
@@ -0,0 +1,6 @@
+;; Transformation of original javascript function from board.js to respective code fragment written in clojure:
+;; hasSafeTiles() {
+;;  return this._numberOfTiles !== this._numberOfBombs;
+;;}
+
+(defn hasSafeTiles  []  (not= int1 int2) )
